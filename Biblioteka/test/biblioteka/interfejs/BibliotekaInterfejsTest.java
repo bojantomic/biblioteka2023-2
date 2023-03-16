0a11f4c4bc114c50b9f032a281ba79439c532289
bojantomic <tomicb@fon.rs>
2023-03-16 11:45:37 +0100

    Testirana klasa Knjiga

--- Biblioteka/test/biblioteka/interfejs/BibliotekaInterfejsTest.java
+++ Biblioteka/test/biblioteka/interfejs/BibliotekaInterfejsTest.java
@@ -5,11 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
